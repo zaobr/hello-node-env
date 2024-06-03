fff69b1b1f07795790701c616a9d4c7457701f92
Mehdi Ouhab <ouhab.me@gmail.com>
2024-06-03 12:13:31 +0200

    Hello env

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+require("dotenv").config();
+
+const data = ["lorem", "ipsum", "dolor", "sit", "amet"];
+
+
+app.get("/words", (req, res) => {
+  res.json(data);
+});
+
+const port = 3310;
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
+console.log(`I am ${process.env.MY_NAME}, wilder in ${process.env.MY_CITY}, and I love ${process.env.MY_LANGUAGE}`)
