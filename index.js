c1b449ddc0fe0668b1c70bbd45aa0899a1b4411c
Mehdi Ouhab <ouhab.me@gmail.com>
2024-06-03 12:43:20 +0200

    file ignored

--- index.js
+++ index.js
@@ -15,4 +15,4 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
 
-console.log(`I am ${process.env.MY_NAME}, wilder in ${process.env.MY_CITY}, and I love ${process.env.MY_LANGUAGE}`)
+console.log(`I am ${process.env.MY_NAME}, wilder in ${process.env.MY_CITY}, and I love ${process.env.MY_LANGUAGE}.`)
